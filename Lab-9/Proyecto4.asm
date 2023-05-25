a8bf35e4e775a3d8b20bc06458c771eb57e3f7cb
Anaru03 <dele22428@uvg.edu.gt>
2023-05-25 11:04:03 -0600

    Configuracion

---
+++ Lab-9/Proyecto4.asm
@@ -0,0 +1,37 @@
+; -----------------------------------------------
+; UNIVERSIDAD DEL VALLE DE GUATEMALA 
+; Organización de computadoras y Assembler
+; Ciclo 1 - 2023
+; Nombre: subrutinas1.asm
+; Descripción: demuestra uso de subrutinas en el mismo 
+;			   documento
+; Autor: KB, May 2023
+; ----------------------------------------------- 
+
+.386
+.model flat, stdcall, c
+.stack 4096
+; ----------------------------------------------- 
+; SECCION DE DECLARACIÓN DE VARIABLES
+; ----------------------------------------------- 
+.data
+ 
+
+; ----------------------------------------------- 
+; SECCION DE DEFINICIÓN DE CÓDIGO
+; ----------------------------------------------- 
+; ------------ Librerías utilizadas -------------
+.code
+    
+	
+
+; ------------ Rutina Principal -------------
+public main
+main proc
+   
+
+
+main endp
+
+
+end
